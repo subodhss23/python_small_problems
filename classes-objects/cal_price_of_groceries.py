b59dda5272d9f8bef8c60f6b11451a34bfaf23f0
subodhss23 <subodhss23@gmail.com>
2020-11-22 16:03:31 -0800

    get in

---
+++ classes-objects/cal_price_of_groceries.py
@@ -0,0 +1,9 @@
+''' Create a function that takes a list of dictionaries(gorceries) which calculates
+    the total price and returns it as a number. A grocery dictionary has a product,
+    a quantity and a price, for example:
+
+    {
+        "product": "Milk,
+        "quantity": 1,
+        "price": 1.50
+    }
