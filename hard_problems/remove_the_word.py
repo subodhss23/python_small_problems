8afc585e21ab2cf2a2247eba881ba8d0cffcdbf8
subodhss23 <subodhss23@gmail.com>
2020-11-25 14:28:14 -0800

    slow

---
+++ hard_problems/remove_the_word.py
@@ -0,0 +1,13 @@
+''' Create a function that takes a list and string. The function should remove the letters in the string from the list,
+    and return the list. '''
+
+def remove_letters(letters, word):
+    new_lst = []
+    for i in letters:
+        if i not in word:
+            letters.remove(i)
+    return letters
+
+print(remove_letters(["s", "t", "r", "i", "n", "g", "w"], "string"))
+print(remove_letters(["b", "b", "l", "l", "g", "n", "o", "a", "w"], "balloon"))
+print(remove_letters(["d", "b", "t", "e", "a", "i"], "edabit"))
