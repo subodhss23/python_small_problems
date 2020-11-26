46490fcb2b1f820f87e40555db5cc013591ed433
subodhss23 <subodhss23@gmail.com>
2020-11-26 10:25:42 -0800

    get in

--- hard_problems/how_many_prime_nums.py
+++ hard_problems/how_many_prime_nums.py
@@ -1,13 +1,16 @@
 '''Create a function that tinds how many prime numbers there are, up to the given integer. '''
 
 def prime_numbers(num):
-    count = 0
-    prime_list = []
-    for i in range(2,10+1):
-        for j in range(1, i+1):
-            if i % j == 0:
-                prime_list.count(j)
-    return prime_list
+	cnt = 0
+	for i in range(2,num):
+		prime = True
+		for j in range(2,i):
+			if i%j == 0:
+				prime = False
+				break
+		if prime:
+			cnt += 1
+	return cnt
 
 print(prime_numbers(10))
 print(prime_numbers(20))
